6e6065525f0d87c33bc590394f241a634df8235f
Kaleb Odle <15kodle@gmail.com>
2020-02-17 17:31:27 -0600

    The sad, sad attempt

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-const bill_total = document.getElementById("bill_total"); 
-const tip_percentage = document.getElementById("tip_percentage");
-const calculate_btn = document.getElementById("calculate_btn");
-const grand_total = document.getElementById("grand_total");
-
-calculate_btn.addEventListener("click",billTotal);
-
 function billTotal(){
-    grand_total.innerHTML(bill_total)
+    const bill_total = document.getElementById("bill_total"); 
+    const tip_percentage = document.getElementById("tip_percentage");
+    const calculate_btn = document.getElementById("calculate_btn");
+    const grand_total = document.getElementById("grand_total");
+
+    bill_total1 = Number(bill_total.value);
+
+    let tax = bill_total1 * 0.055;
+    let taxed_Total = bill_total1 +tax;
+    let tip_Amount = bill_total1 * tip_percentage.value/100;
+    let end_Total = (taxed_Total + tip_Amount);
+
+    grand_total.innerHTML = `Grand Total: $${end_Total}`;
 }
+
+function round(value, decimals) {
+    return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+  }
